d9185ab43f7c364de1f7580d3e0ce174ec9679ab
hartkkuh <a217287499@gmail.com>
2025-03-10 20:11:43 +0200

    add_vacations_app

--- reactproject/myapp/src/components/navbarapp/navbarapp.tsx
+++ reactproject/myapp/src/components/navbarapp/navbarapp.tsx
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/authcontext'
 import css from "./navbarapp.module.css"
 
 export function Navbarapp():JSX.Element{
+    const myContext = useContext(AuthContext);
     return (
         <div>    
             <nav className={css.container}>
                 <Link className={css.link} to="/">Home</Link>
-                <Link className={css.link} to="/register">Register</Link>
-                <Link className={css.link} to="/login">Login</Link>
-                <Link className={css.link} to="/vacations">Vacations</Link>
-                <Link className={css.link} to="/logout">Logout</Link>
-                <Link className={css.link} to="/updetmyuser">updet my user</Link>
-                <Link className={css.link} to="/userinformation">User Information</Link>
-                <Link className={css.link} to="/selectuser">Select user</Link>
-                <Link className={css.link} to="/statistics">statistics</Link>
+                {!myContext?.token && <Link className={css.link} to="/register">Register</Link>}
+                {!myContext?.token && <Link className={css.link} to="/login">Login</Link>}
+                {myContext?.token && <Link className={css.link} to="/vacations">Vacations</Link>}
+                {myContext?.token && <Link className={css.link} to="/updetmyuser">updet my user</Link>}
+                {myContext?.is_admin && <Link className={css.link} to="/userinformation">User Information</Link>}
+                {myContext?.is_admin && <Link className={css.link} to="/selectuser">Select user</Link>}
+                {myContext?.token && <Link className={css.link} to="/statistics">statistics</Link>}
+                {myContext?.token && <Link className={css.link} to="/logout">Logout</Link>}
 
             </nav>
         </div>
